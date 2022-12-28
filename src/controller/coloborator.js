05d007cde2870badc0fea596d62d509d4319fec1
Claylton Roque <claylton202@gmail.com>
2022-12-28 14:10:45 -0300

    fix/ vercel

--- src/controller/coloborator.js
+++ src/controller/coloborator.js
@@ -1,110 +1,97 @@
 import { PrismaClient } from "@prisma/client";
-import Jwt  from "jsonwebtoken";
-
+import Jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
 export async function selectColoboradores(req, res) {
-    const Coloboradores = await prisma.coloborador.findMany({
-        orderBy:{id:"asc"}})
-        return res.status(200).json(Coloboradores)
+  const Coloboradores = await prisma.coloborador.findMany({
+    orderBy: { id: "asc" },
+  });
+  return res.status(200).json(Coloboradores);
 }
 
 export async function insertColoborador(req, res) {
-    const {name, admission, login, password, role, type_blood} = req.body;
-    if(!name || !admission || !login || !password || !role || !type_blood){
-        return res.status(400).json("Mande todas as variaveis")
-    }
-    const coloboradorInsert = await prisma.coloborador.create({
-        data: {
-            name,
-            admission, 
-            role,
-            type_blood,
-            login,
-            password,
-        }
-    })
-    return res.status(201).json(coloboradorInsert)
+  const { name, admission, login, password, role, type_blood } = req.body;
+  if (!name || !admission || !login || !password || !role || !type_blood) {
+    return res.status(400).json("Mande todas as variaveis");
+  }
+  const coloboradorInsert = await prisma.coloborador.create({
+    data: {
+      name,
+      admission,
+      role,
+      type_blood,
+      login,
+      password,
+    },
+  });
+  return res.status(201).json(coloboradorInsert);
 }
 
 export async function updateColoborador(req, res) {
-    const {name, admission, login, password, role, type_blood} = req.body;
-    if(!name || !admission || !login || !password || !role || type_blood){
-        return res.status(400).json("Mande todas as variaveis")
-    }
-    const coloboradorUpdate = await prisma.coloborador.update({
-     where: { id },
-     data: {
-         name,
-         admission,
-         role,
-         type_blood,
-         login,
-         password,
-     }
-    });
-    return res.status(200).json(coloboradorUpdate)
+  const { name, admission, login, password, role, type_blood } = req.body;
+  if (!name || !admission || !login || !password || !role || type_blood) {
+    return res.status(400).json("Mande todas as variaveis");
+  }
+  const coloboradorUpdate = await prisma.coloborador.update({
+    where: { id },
+    data: {
+      name,
+      admission,
+      role,
+      type_blood,
+      login,
+      password,
+    },
+  });
+  return res.status(200).json(coloboradorUpdate);
 }
 
 export async function deleteColoborador(req, res) {
   const { id } = req.body;
-  const intId = parseInt(id)
+  const intId = parseInt(id);
 
-  if(!intId){
-    return res.status(400).json("Id require")
+  if (!intId) {
+    return res.status(400).json("Id require");
+  }
+
+  const coloboratorExist = await prisma.coloborador.findUnique({
+    where: { id: intId },
+  });
+
+  if (!coloboratorExist) {
+    return res.status(404).json("coloborator not find");
+  }
+
+  await prisma.coloborador.delete({
+    where: { id: intId },
+  });
+
+  return res.status(200).send();
 }
 
-const coloboratorExist = await prisma.coloborador.findUnique({
-    where: { id: intId } 
-});
+export async function coloborador_Authenticate(req, res) {
+  const { login, password } = req.body;
+  if (!login) {
+    return res.status(400).json("Login is mandatory");
+  }
+  const coloboradorExist = await prisma.coloborador.findFirst({
+    where: {
+      login,
+    },
+  });
 
-if(!coloboratorExist) {
-    return res.status(404).json("coloborator not find")
+  if (!coloboradorExist) {
+    return res.status(400).json("coloborator not exist");
+  }
+  console.log(password, coloboradorExist.password);
+  if (password !== coloboradorExist.password) {
+    return res.status(403).json("coloborator_name or coloborator_senha wrong");
+  }
+
+  const token = Jwt.sign(coloboradorExist, "SECRETKEY", {
+    expiresIn: "1d",
+  });
+
+  return res.json({ auth: true, token: token, colaborador: coloboradorExist });
 }
-
-await prisma.coloborador.delete({
-    where: {id: intId}
-});
-
-return res.status(200).send();
-
-} 
-
-export async  function coloborador_Authenticate (req, res) {
-    const { login, password } = req.body;
-    if(!login){
-        return res.status(400).json("Login is mandatory")
-    }
-    const coloboradorExist = await prisma.coloborador.findFirst({
-        where: {
-            login
-        }   
-    })
-
-    if(!coloboradorExist){
-        return res.status(400).json("coloborator not exist")
-    }
-    console.log(password, coloboradorExist.password)
-    if(password !== coloboradorExist.password){
-        return res.status(403).json("coloborator_name or coloborator_senha wrong")
-    }
-
-    const token = Jwt.sign(coloboradorExist, "SECRETKEY", {
-        expiresIn: "1d" 
-    })
-
-    return res.json({ auth: true, token: token, colaborador: coloboradorExist})
-
-}
-
-
-
-
-
-
-
-
-
-
-
--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,43 @@
 import { Router } from "express";
-import { selectColoboradores, insertColoborador, updateColoborador, deleteColoborador, coloborador_Authenticate} from "./controller/coloborator.js";
-import {insertService, deleteService, finalizeService, attendenceService, selectServices} from './controller/services.js'
+import {
+  selectColoboradores,
+  insertColoborador,
+  updateColoborador,
+  deleteColoborador,
+  coloborador_Authenticate,
+} from "./controller/coloborator.js";
+import {
+  insertService,
+  deleteService,
+  finalizeService,
+  attendenceService,
+  selectServices,
+} from "./controller/services.js";
 import { correctJwt } from "./auth.js";
 
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.json({
-        "statusCode": 200,
-        "msg": "Api Rodando"
-    })
-})
+router.get("/", (req, res) => {
+  res.json({
+    statusCode: 200,
+    msg: "Api Rodando",
+  });
+});
 
-router.get('/coloboradores', correctJwt, selectColoboradores)
-router.post('/coloborador', insertColoborador)
-router.put('/coloborador',correctJwt, updateColoborador)
-router.delete('/coloborador',correctJwt, deleteColoborador)
+router.get("/coloboradores", correctJwt, selectColoboradores);
+router.post("/coloborador", insertColoborador);
+router.put("/coloborador", correctJwt, updateColoborador);
+router.delete("/coloborador", correctJwt, deleteColoborador);
 
-router.get('/services',  selectServices)
-router.post('/services', correctJwt, insertService)
-router.delete('/services',correctJwt, deleteService)
-
+router.get("/services", selectServices);
+router.post("/services", correctJwt, insertService);
+router.delete("/services", correctJwt, deleteService);
 
 // Authenticate
-router.post('/coloborador_authenticate', coloborador_Authenticate)
+router.post("/coloborador_authenticate", coloborador_Authenticate);
 
 // Finalize service and send email
-router.post('/finalize_service', correctJwt, finalizeService)
-router.post('/attendence_service', attendenceService)
-
+router.post("/finalize_service", correctJwt, finalizeService);
+router.post("/attendence_service", attendenceService);
 
 export default router;
